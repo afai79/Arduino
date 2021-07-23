c10074f2d327e58647b6fa11a9d959c36b95ba4e
Abdelhameed Ibrahim <76248162+afai79@users.noreply.github.com>
2021-07-23 18:46:57 +0300

    Update LCD.ino

--- LCD.ino
+++ LCD.ino
@@ -1,14 +1,5 @@
 /*
   LiquidCrystal Library - Hello World
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD
- and shows the time.
-
   The circuit:
  * LCD RS pin to digital pin 12
  * LCD Enable pin to digital pin 11
@@ -23,38 +14,23 @@
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
 
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystal
  */
 
-// include the library code:
-#include <LiquidCrystal.h>
+#include<LiquidCrystal.h>
 
-// initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
-void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
+void setup()
+{
+ lcd.begin(16, 2); 
 }
 
-void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+void loop()
+{
+  lcd.setCursor(1,0);          
+  lcd.print("Arduino"); 
+  lcd.setCursor(2,1);           
+  lcd.print("Uno");    
+  delay(4000);
+  lcd.clear();
+  delay(5000);
 }
- 
