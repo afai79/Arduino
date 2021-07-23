31a39838ebde0c7d31d5dbc522ddd6ede403887c
Abdelhameed Ibrahim <76248162+afai79@users.noreply.github.com>
2021-07-23 18:30:16 +0300

    Add files via upload

---
+++ LCD_I2C.ino
@@ -0,0 +1,15 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2);  
+void setup()
+{  lcd.init();                      // initialize the lcd 
+  lcd.init();
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Hello, world!");
+  lcd.setCursor(0,1);
+  lcd.print("Arduino!");
+}
+void loop()
+{ }
